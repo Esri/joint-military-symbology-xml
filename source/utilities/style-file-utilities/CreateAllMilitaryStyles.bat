0d44c34d18f10310eb372dbadf4f4e772105c794
C Moore <csmooreacm@acm.org>
2014-04-15 20:13:04 -0400

    Partial #21 (add converters) - image files csvs
    Added sample ImageFileNameCategoryTags Tables + .bat creator script -
    These are sample/rough draft ImageFileNameCategoryTags tables in folder
    imagefile_name_category_tags - just to create a placeholder for these &
    to see what these will look like & get feedback & get filenames
    established (that are needed for later/readme)

---
+++ source/utilities/style-file-utilities/CreateAllMilitaryStyles.bat
@@ -0,0 +1,30 @@
+:: !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+:: IMPORTANT/TODO: you must set/correct paths below
+::
+:: IMPORTANT CONSTRAINT: 
+:: 1. Must be FULL PATHs (because the replacement below depends on)
+:: 2. No spaces in the folder/file names (doing text replacement of variables in DOS is just too impossible)
+:: {TODO_NO_SPACES}
+:: !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+:: Military-Activities-Source-Icons.csv
+:: Military-Air-Source-Icons.csv
+:: Military-ControlMeasures-Source-Icons.csv
+:: Military-Cyberspace-Source-Icons.csv
+:: Military-Land-Source-Icons.csv
+:: Military-Sea-Source-Icons.csv
+:: Military-Sigint-Source-Icons.csv
+:: Military-Space-Source-Icons.csv
+:: Military-Weather-Source-Icons.csv
+
+csv2ArcGISStyle.exe {TODO_NO_SPACES}\Military-Activities-Source-Icons.csv {TODO_NO_SPACES}\Military-Activities.style
+csv2ArcGISStyle.exe {TODO_NO_SPACES}\Military-Air-Source-Icons.csv {TODO_NO_SPACES}\Military-Air.style
+csv2ArcGISStyle.exe {TODO_NO_SPACES}\Military-ControlMeasures-Source-Icons.csv {TODO_NO_SPACES}\Military-ControlMeasures.style
+csv2ArcGISStyle.exe {TODO_NO_SPACES}\Military-Cyberspace-Source-Icons.csv {TODO_NO_SPACES}\Military-Cyberspace.style
+csv2ArcGISStyle.exe {TODO_NO_SPACES}\Military-Land-Source-Icons.csv {TODO_NO_SPACES}\Military-Land.style
+csv2ArcGISStyle.exe {TODO_NO_SPACES}\Military-Sea-Source-Icons.csv {TODO_NO_SPACES}\Military-Sea.style
+csv2ArcGISStyle.exe {TODO_NO_SPACES}\Military-Sigint-Source-Icons.csv {TODO_NO_SPACES}\Military-Sigint.style
+csv2ArcGISStyle.exe {TODO_NO_SPACES}\Military-Weather-Source-Icons.csv {TODO_NO_SPACES}\Military-Weather.style
+csv2ArcGISStyle.exe {TODO_NO_SPACES}\Military-Space-Source-Icons.csv {TODO_NO_SPACES}\Military-Space.style
+
+
