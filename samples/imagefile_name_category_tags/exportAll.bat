44218f58c68b9dbd160c3ae916d0aa09c2b5d363
Andy Bouffard <abouffard@esri.com>
2014-05-07 15:28:35 -0500

    New csv files and a better batch file
    Changed the regular expressions in the batch files and produced new
    output.

--- samples/imagefile_name_category_tags/exportAll.bat
+++ samples/imagefile_name_category_tags/exportAll.bat
@@ -1,12 +1,12 @@
 setlocal
 cd ..\..\source\JointMilitarySymbologyLibraryCS\jmsml\bin\Debug
 jmsml.exe /xi="../../../../../samples/imagefile_name_category_tags/Military-Activities-Source-Icons-Test" /s="Activities" /+
-jmsml.exe /xi="../../../../../samples/imagefile_name_category_tags/Military-Air-Source-Icons-Test" /s="Air" /+
-jmsml.exe /xi="../../../../../samples/imagefile_name_category_tags/Military-Cyberspace-Source-Icons-Test" /s="Cyberspace" /+
-jmsml.exe /xi="../../../../../samples/imagefile_name_category_tags/Military-Land-Source-Icons-Test" /s="Land" /+
-jmsml.exe /xi="../../../../../samples/imagefile_name_category_tags/Military-Sea-Source-Icons-Test" /s="Sea" /+
-jmsml.exe /xi="../../../../../samples/imagefile_name_category_tags/Military-Space-Source-Icons-Test" /s="Space" /+
+jmsml.exe /xi="../../../../../samples/imagefile_name_category_tags/Military-Air-Source-Icons-Test" /s="^Air|Air Missile$" /+
+jmsml.exe /xi="../../../../../samples/imagefile_name_category_tags/Military-Cyberspace-Source-Icons-Test" /s="^Cyberspace$" /+
+jmsml.exe /xi="../../../../../samples/imagefile_name_category_tags/Military-Land-Source-Icons-Test" /s="^Land Units|Land Civilian|Land Equipment|Land Installation$" /+
+jmsml.exe /xi="../../../../../samples/imagefile_name_category_tags/Military-Sea-Source-Icons-Test" /s="^Sea Surface|Sea Subsurface|Mine Warfare$" /+
+jmsml.exe /xi="../../../../../samples/imagefile_name_category_tags/Military-Space-Source-Icons-Test" /s="^Space|Space Missile$" /+
 jmsml.exe /xi="../../../../../samples/imagefile_name_category_tags/Military-Sigint-Source-Icons-Test" /s="Signals Intelligence" /+
-jmsml.exe /xi="../../../../../samples/imagefile_name_category_tags/Military-ControlMeasures-Source-Icons-Test" /s="Control Measures" /+
+jmsml.exe /xi="../../../../../samples/imagefile_name_category_tags/Military-ControlMeasures-Source-Icons-Test" /s="^Control Measures$" /+
 jmsml.exe /xi="../../../../../samples/imagefile_name_category_tags/Military-Weather-Source-Icons-Test" /s="Atmospheric|Oceanographic|Meteorological Space" /+
 endlocal
