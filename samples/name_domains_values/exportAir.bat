0f1c281703652b25ef81b91e500a6621baf67f7a
Chris Moore <csmooreacm@acm.org>
2016-02-26 15:48:34 -0500

    Initial cut of 2525C(Charlie) Military Overlay schema

--- samples/name_domains_values/exportAir.bat
+++
@@ -1,4 +0,0 @@
-setlocal
-cd ..\..\source\JointMilitarySymbologyLibraryCS\jmsml\bin\Debug
-jmsml.exe /xe="../../../../../samples/name_domains_values/Air_Coded_Domain_Sample" /s="^Air$" /xas="DOMAIN" /+
-endlocal
--- samples/name_domains_values/exportLightAir.bat
+++
@@ -1,4 +0,0 @@
-setlocal
-cd ..\..\source\JointMilitarySymbologyLibraryCS\jmsml\bin\Debug
-jmsml.exe /xe="../../../../../samples/name_domains_values/LightAir_Coded_Domain_Sample" /s="^Air$" /q="Light" /xas="DOMAIN" /+
-endlocal
